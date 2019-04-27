ec8dbeb0e794b7c3955676acbb8051d71d66bb04
Brandon <BrandonPoole@live.ca>
2019-04-27 17:53:20 -0400

    Improved Styles & Navbar
    - Added Bootstrap 4.3
    - Implemented Bootstrap navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './common/home.component';
+import { SandboxComponent } from './sandbox/sandbox.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch:'full'},
-  {path: 'home', component:HomeComponent}
+  {path: 'home', component:HomeComponent},
+  {path: 'sandbox', component:SandboxComponent}
 ];
 
 @NgModule({
