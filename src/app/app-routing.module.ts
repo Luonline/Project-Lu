46ed53804b34195a8c488f44b32f8b874704d26e
Brandon <BrandonPoole@live.ca>
2019-04-28 22:02:57 -0400

    Experience Tab
    - Start of Experience
    - Replace Google icons with Font Awesome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './common/home.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch:'full'},
   {path: 'home', component:HomeComponent},
-  {path: 'sandbox', component:SandboxComponent}
+  {path: 'sandbox', component:SandboxComponent},
+  {path: 'experience', component:ExperienceComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatChipsModule} from '@angular/material';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { HomeComponent } from './common/home.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SandboxComponent,
-    HomeComponent //common
+    HomeComponent,
+    ExperienceComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule, //Start Angular Material imports
-    MatCheckboxModule
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatChipsModule
     
   ],
   providers: [],
